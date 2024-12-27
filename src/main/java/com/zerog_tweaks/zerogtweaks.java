a7f5dae8cd80bf89669d473201534e2458e84c29
MrWhiteFlamesYT <swiegersjj1@gmail.com>
2024-12-27 09:17:42 +0200

    Fixed class and file name mismatch by renaming to ZeroGTweaks. Updated all references from zerogtweak to ZeroGTweaks for consistency. Ensured proper registration and creative tab integration for items and blocks.

--- src/main/java/com/zerog_tweaks/zerogtweaks.java
+++ src/main/java/com/zerog_tweaks/zerogtweaks.java
@@ -30,8 +30,8 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-@Mod(ZeroGTweak.MODID)
-public class ZeroGTweak {
+@Mod(ZeroGTweaks.MODID)
+public class ZeroGTweaks {
     public static final String MODID = "zerogtweaks";
     private static final Logger LOGGER = LogUtils.getLogger();
 
@@ -41,14 +41,16 @@ public class ZeroGTweak {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     // Block and Item Definitions
-    public static final DeferredBlock<Block> AUSIOUM_BLOCK = BLOCKS.registerSimpleBlock("ausioum_block", 
-        BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-    public static final DeferredItem<BlockItem> AUSIOUM_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("ausioum_block", AUSIOUM_BLOCK);
-    public static final DeferredItem<Item> AUSIOUM_ITEM = ITEMS.registerSimpleItem("ausioum_item", 
-        new Item.Properties());
+    public static final DeferredBlock<Block> AUSIOUM_BLOCK = BLOCKS.register("ausioum_block", 
+        () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
+    public static final DeferredItem<BlockItem> AUSIOUM_BLOCK_ITEM = ITEMS.register("ausioum_block", 
+        () -> new BlockItem(AUSIOUM_BLOCK.get(), new Item.Properties()));
+    public static final DeferredItem<Item> AUSIOUM_ITEM = ITEMS.register("ausioum_item", 
+        () -> new Item(new Item.Properties()));
 
     // Custom Creative Tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ZEROG_TWEAKS_TAB = CREATIVE_MODE_TABS.register("zerogtweaks_tab", () -> CreativeModeTab.builder()
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ZEROG_TWEAKS_TAB = CREATIVE_MODE_TABS.register("zerogtweaks_tab", 
+        () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.zerogtweaks_tab"))
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> AUSIOUM_ITEM.get().getDefaultInstance())
@@ -57,7 +59,7 @@ public class ZeroGTweak {
                 output.accept(AUSIOUM_BLOCK_ITEM.get());
             }).build());
 
-    public ZeroGTweak(IEventBus modEventBus, ModContainer modContainer) {
+    public ZeroGTweaks(IEventBus modEventBus, ModContainer modContainer) {
         // Register events and registries
         modEventBus.addListener(this::commonSetup);
         BLOCKS.register(modEventBus);
