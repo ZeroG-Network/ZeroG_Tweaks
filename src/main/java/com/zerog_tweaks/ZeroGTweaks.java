084266611cc051f44ae5a755bba8a5a13aecbfdc
MrWhiteFlamesYT <swiegersjj1@gmail.com>
2024-12-27 10:01:06 +0200

    Refactored code to fully align with NeoForge APIs, replacing all Forge-specific references. Updated DeferredRegister and RegistryObject usage for NeoForge compatibility. Fixed event handling and creative tab integration using NeoForge classes. Added logging for key lifecycle events.

--- src/main/java/com/zerog_tweaks/ZeroGTweaks.java
+++ src/main/java/com/zerog_tweaks/ZeroGTweaks.java
@@ -13,30 +13,32 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.RegistryObject;
 
 @Mod(ZeroGTweaks.MODID)
 public class ZeroGTweaks {
     public static final String MODID = "zerogtweaks";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    // Register Blocks and Items
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(ForgeRegistries.CREATIVE_MODE_TABS, MODID);
+    // Use NeoForge's DeferredRegister
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Block and Item Definitions
+    // Register blocks, items, and tabs
     public static final RegistryObject<Block> AUSIOUM_BLOCK = BLOCKS.register("ausioum_block",
         () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
     public static final RegistryObject<Item> AUSIOUM_BLOCK_ITEM = ITEMS.register("ausioum_block",
@@ -54,28 +56,43 @@ public class ZeroGTweaks {
                 output.accept(AUSIOUM_BLOCK_ITEM.get());
             }).build());
 
-    public ZeroGTweaks() {
-        IEventBus modEventBus = net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext.get().getModEventBus();
+    public ZeroGTweaks(IEventBus modEventBus, ModContainer modContainer) {
+        // Register events and registries
+        modEventBus.addListener(this::commonSetup);
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         CREATIVE_MODE_TABS.register(modEventBus);
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addItemsToCreativeTabs);
 
-        modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(this::clientSetup);
-
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("Common setup initialized for {}", MODID);
     }
 
-    private void clientSetup(final FMLClientSetupEvent event) {
-        LOGGER.info("Client setup initialized for {}", MODID);
+    private void addItemsToCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        // Add items to appropriate creative tabs
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(AUSIOUM_BLOCK_ITEM.get());
+        }
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(AUSIOUM_ITEM.get());
+        }
     }
 
     @SubscribeEvent
     public static void onServerStarting(ServerStartingEvent event) {
         LOGGER.info("Server starting for mod {}", MODID);
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("Client setup initialized for {}", MODID);
+            LOGGER.info("Player name: {}", Minecraft.getInstance().getUser().getName());
+        }
     }
 }
